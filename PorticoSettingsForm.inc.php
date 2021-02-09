4b1503cf5a91a64163061eb4230eae21e20876b3
Alec Smecher <alec@smecher.bc.ca>
2021-02-09 11:43:19 -0800

    Code headers and copyright dates

--- PorticoSettingsForm.inc.php
+++ PorticoSettingsForm.inc.php
@@ -1,14 +1,13 @@
 <?php
 
 /**
- * @file plugins/importexport/portico/PorticoSettingsForm.inc.php
+ * @file PorticoSettingsForm.inc.php
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2003-2020 John Willinsky
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * @class PorticoSettingsForm
- * @ingroup plugins_importexport_portico
  *
  * @brief Form for journal managers to modify Portico plugin settings
  */
--- index.php
+++ index.php
@@ -5,13 +5,12 @@
  */
 
 /**
- * @file plugins/importexport/portico/index.php
+ * @file index.php
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2003-2020 John Willinsky
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
- * @ingroup plugins_importexport_portico
  * @brief Wrapper for portico XML export plugin.
  *
  */
